883503af5aa3e8506e46ce7ca10a0275269f2cfe
Kiril-Pavlov <Kire_P@yahoo.com>
2022-08-08 20:26:14 +0200

    created components. Home component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import Nav from './components/Nav';
+import Features from './components/Features';
+import Download from './components/Download';
+import Questions from './components/Questions';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Home />
+      <Features />
+      <Download />
+      <Questions />
+      <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import homeImg from "../assets/illustration-hero.svg"
+import "../components/Home.css"
+
+const Home = () => {
+  return (
+    <div className='home-container'>
+            <div className='home-content'>
+                <h1 className='home-title'>A Simple Bookmark Manager</h1>
+                <p>A clean and simple interface to organize your favourite websites. Open a new browser tab and see your sites load instantly. Try it for free.</p>
+                <button className='home-chrome-button'>Get it on Chrome</button>
+                <button className='home-firefox-button'>Get it on Firefox</button>
+            </div>
+            <div className='home-image'>
+                <img src={homeImg} alt="Home Image" />
+                <div className='poligon'></div>
+            </div>
+    </div>
+  )
+}
+
+export default Home
